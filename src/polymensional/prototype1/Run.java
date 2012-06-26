d26d74a884dc0cc3f585f51f3fe96e5a19931a41
Zach Ewert <zachanima@gmail.com>
2012-06-26 16:15:01 +0200

    Full screen, no title

--- src/polymensional/prototype1/Run.java
+++ src/polymensional/prototype1/Run.java
@@ -2,12 +2,20 @@ package polymensional.prototype1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Run extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    
+    // Full screen.
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    
+    
+    setContentView(R.layout.main);
+  }
 }

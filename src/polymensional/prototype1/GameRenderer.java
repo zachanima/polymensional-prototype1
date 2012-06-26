d7d5ce7614e6337a18a66b4576c3ca806d720695
Zach Ewert <zachanima@gmail.com>
2012-06-26 17:42:46 +0200

    Add entity class

--- src/polymensional/prototype1/GameRenderer.java
+++ src/polymensional/prototype1/GameRenderer.java
@@ -4,12 +4,13 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
-import android.opengl.GLU;
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLU;
 
 public class GameRenderer implements Renderer {
   public GameRenderer(Context context) {
     this.context = context;
+    this.entity = new Entity();
   }
 
 
@@ -18,6 +19,8 @@ public class GameRenderer implements Renderer {
     gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
     gl.glLoadIdentity();
     gl.glTranslatef(0.0f, 0.0f, -20.0f);
+    
+    entity.draw(gl);
   }
 
 
@@ -38,13 +41,16 @@ public class GameRenderer implements Renderer {
 
   @Override
   public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-     gl.glEnable(GL10.GL_TEXTURE_2D);
-     gl.glShadeModel(GL10.GL_SMOOTH);
-     gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
-     gl.glEnable(GL10.GL_BLEND);
-     gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+    entity.loadGLTexture(gl, this.context, R.drawable.ic_launcher);
+    
+    gl.glEnable(GL10.GL_TEXTURE_2D);
+    gl.glShadeModel(GL10.GL_SMOOTH);
+    gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+    gl.glEnable(GL10.GL_BLEND);
+    gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
   }
   
   
   private Context context;
+  private Entity entity;
 }

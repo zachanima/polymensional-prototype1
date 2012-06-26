47a47d921cca9418241ce156eae442fa07ecf79c
Zach Ewert <zachanima@gmail.com>
2012-06-27 00:34:35 +0200

    Use absolute values for width and height for ortho

--- src/polymensional/prototype1/GameRenderer.java
+++ src/polymensional/prototype1/GameRenderer.java
@@ -66,7 +66,7 @@ public class GameRenderer implements Renderer {
     gl.glMatrixMode(GL10.GL_PROJECTION);
     gl.glPushMatrix();
     gl.glLoadIdentity();
-    gl.glOrthof(0.0f, (float)viewport.get(2), 0.0f, (float)viewport.get(3), -1.0f, 1.0f);
+    gl.glOrthof(0.0f, 1280.0f, 0.0f, 720.0f, -1.0f, 1.0f);
     gl.glMatrixMode(GL10.GL_MODELVIEW);
     gl.glPushMatrix();
     gl.glLoadIdentity();

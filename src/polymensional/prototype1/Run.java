76343f2cea5b2c0a02239fc21ea93f2e7de89e46
Zach Ewert <zachanima@gmail.com>
2012-06-26 16:25:42 +0200

    Remove whitespace

--- src/polymensional/prototype1/Run.java
+++ src/polymensional/prototype1/Run.java
@@ -9,12 +9,8 @@ public class Run extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
     requestWindowFeature(Window.FEATURE_NO_TITLE);
-    
-    // Full screen.
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    
     setContentView(R.layout.main);
   }
 }
